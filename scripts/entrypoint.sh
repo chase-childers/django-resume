6654c27f0515c9a6b8f4a33d308a88bc02520293
Chase Childers <24755648+chase-childers@users.noreply.github.com>
2019-12-11 22:50:23 -0800

    Update to Docker container

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,11 +4,11 @@ touch /srv/logs/gunicorn.log
 touch /srv/logs/access.log
 tail -n 0 -f /srv/logs/*.log &
 
+exec service nginx start &
 exec pipenv run gunicorn \
     --bind :8000 \
     --log-level=info \
     --log-file=/srv/logs/gunicorn.log \
     --access-logfile=/srv/logs/access.log \
-    backend.wsgi:application --pythonpath backend &
+    backend.wsgi:application --pythonpath backend 
 
-exec service nginx start
